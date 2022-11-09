d9998c0f90eed608547170c6aec5cc89c63329b5
Cody Cooper <cody_codster@live.com>
2022-11-08 18:58:55 -0600

    Updated localStorage functionality via Javascript

--- Develop/script.js
+++ Develop/script.js
@@ -5,14 +5,48 @@ $(function () {
 
   var today = dayjs(); //Variable for todays date
   $('#currentDay').text(today.format('dddd, MMMM DD')); //Display current date
+  var currentTime = dayjs().hour(); //Gets hour not including minutes. 2:56pm will be 14  
 
-  var currentTime = dayjs().hour(); //Gets hour not including minutes. 2:56pm will be 14
-      //Saved code  
+//Event listeners for each work hour 9am - 5pm !HELP REPEAT CODE
+  $('#SB9').click(function() {
+    localStorage.setItem('9am', $('#9am').val());
+  })
 
-  var hourBox = [$('#hour-9'), $('#hour-10'), $('#hour-11'), $('#hour-12'), $('#hour-13'), $('#hour-14'), $('#hour-15'), $('#hour-16'), $('#hour-17')];    
-  console.log(currentTime);
-  console.log(typeof currentTime);
+  $('#SB10').click(function() {
+    localStorage.setItem('10am', $('#10am').val());
+  })
 
+  $('#SB11').click(function() {
+    localStorage.setItem('11am', $('#11am').val());
+  })
+
+  $('#SB12').click(function() {
+    localStorage.setItem('12pm', $('#12pm').val());
+  })
+
+  $('#SB1').click(function() {
+    localStorage.setItem('1pm', $('#1pm').val());
+  })
+
+  $('#SB2').click(function() {
+    localStorage.setItem('2pm', $('#2pm').val());
+  })
+
+  $('#SB3').click(function() {
+    localStorage.setItem('3pm', $('#3pm').val());
+  })
+
+  $('#SB4').click(function() {
+    localStorage.setItem('4pm', $('#4pm').val());
+  })
+
+  $('#SB5').click(function() {
+    localStorage.setItem('5pm', $('#5pm').val());
+  })
+
+
+
+//To check if event. target has specific class, call the classList. contains() method on the target object, e.g. event.
 
   //Fills in text boxes with appropriate colors !REAPEAT CODE NEEDS FIXED
   if (currentTime == 9) {
@@ -87,8 +121,61 @@ $(function () {
     $('#hour-5').addClass('future');
   }
 
-  
-  console.log(currentTime);
+//Grabs localStorage keys and plugs them in if they exist !HELP REPEAT CODE 
+
+if (window.localStorage.getItem('9am') !== null) { //Checks if 9am key exists
+  var text = window.localStorage.getItem('9am');
+  console.log(text);
+  $('#9am').text(text);
+}
+
+if (window.localStorage.getItem('10am') !== null) { //Checks if 10am key exists
+  var text = window.localStorage.getItem('10am');
+  console.log(text);
+  $('#10am').text(text);
+}
+
+if (window.localStorage.getItem('11am') !== null) { //Checks if 11am key exists
+  var text = window.localStorage.getItem('11am');
+  console.log(text);
+  $('#11am').text(text);
+}
+
+if (window.localStorage.getItem('12pm') !== null) { //Checks if 12am key exists
+  var text = window.localStorage.getItem('12pm');
+  console.log(text);
+  $('#12pm').text(text);
+}
+
+if (window.localStorage.getItem('1pm') !== null) { //Checks if 1pm key exists
+  var text = window.localStorage.getItem('1pm');
+  console.log(text);
+  $('#1pm').text(text);
+}
+
+if (window.localStorage.getItem('2pm') !== null) { //Checks if 2pm key exists
+  var text = window.localStorage.getItem('2pm');
+  console.log(text);
+  $('#2pm').text(text);
+}
+
+if (window.localStorage.getItem('3pm') !== null) { //Checks if 3pm key exists
+  var text = window.localStorage.getItem('3pm');
+  console.log(text);
+  $('#3pm').text(text);
+}
+
+if (window.localStorage.getItem('4pm') !== null) { //Checks if 4pm key exists
+  var text = window.localStorage.getItem('4pm');
+  console.log(text);
+  $('#4pm').text(text);
+}
+
+if (window.localStorage.getItem('5pm') !== null) { //Checks if 5pm key exists
+  var text = window.localStorage.getItem('5pm');
+  console.log(text);
+  $('#5pm').text(text);
+}
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
