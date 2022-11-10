734083fb2af8dce1c64edf819ee9c4b75ccc4b75
Cody Cooper <cody_codster@live.com>
2022-11-09 21:17:50 -0600

    Updated README file

--- Develop/script.js
+++ Develop/script.js
@@ -17,46 +17,7 @@ function init () {
     }
   }};
 
-
-  $('#SB9').click(function() {
-    localStorage.setItem('9box', $('#9h').val());
-  })
-
-  $('#SB10').click(function() {
-    localStorage.setItem('10box', $('#10h').val());
-  })
-
-  $('#SB11').click(function() {
-    localStorage.setItem('11box', $('#11h').val());
-  })
-
-  $('#SB12').click(function() {
-    localStorage.setItem('12box', $('#12h').val());
-  })
-
-  $('#SB13').click(function() {
-    localStorage.setItem('13box', $('#13h').val());
-  })
-
-  $('#SB14').click(function() {
-    localStorage.setItem('14box', $('#14h').val());
-  })
-
-  $('#SB15').click(function() {
-    localStorage.setItem('15box', $('#15h').val());
-  })
-
-  $('#SB16').click(function() {
-    localStorage.setItem('16box', $('#16h').val());
-  })
-
-  $('#SB17').click(function() {
-    localStorage.setItem('17box', $('#17h').val());
-
-  })
-
-
-//Fills in text boxes with appropriate colors !REAPEAT CODE NEEDS FIXED
+//Fills in text boxes with appropriate colors
 function boxFill() {
   for (i=9; i<18; i++) {
     if (currentTime == i) {
@@ -71,5 +32,45 @@ function boxFill() {
 
 boxFill();
 init();
+
+//Event listeners for each save button
+$('#SB9').click(function(event) {
+  localStorage.setItem('9box', $('#9h').val());
+})
+
+$('#SB10').click(function() {
+  localStorage.setItem('10box', $('#10h').val());
+})
+
+$('#SB11').click(function() {
+  localStorage.setItem('11box', $('#11h').val());
+})
+
+$('#SB12').click(function() {
+  localStorage.setItem('12box', $('#12h').val());
+})
+
+$('#SB13').click(function() {
+  localStorage.setItem('13box', $('#13h').val());
+})
+
+$('#SB14').click(function() {
+  localStorage.setItem('14box', $('#14h').val());
+})
+
+$('#SB15').click(function() {
+  localStorage.setItem('15box', $('#15h').val());
+})
+
+$('#SB16').click(function() {
+  localStorage.setItem('16box', $('#16h').val());
+})
+
+$('#SB17').click(function() {
+  localStorage.setItem('17box', $('#17h').val());
+
+})
+
+
 
 });
